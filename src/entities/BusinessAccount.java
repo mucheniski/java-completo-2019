848a83ef2e1612f337c52c59a274a5920ea3479b
mucheniski <mucheniski@gmail.com>
2019-08-31 15:27:58 -0300

    Palavra Final

--- src/entities/BusinessAccount.java
+++ src/entities/BusinessAccount.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class BusinessAccount extends Account {
+public final class BusinessAccount extends Account {
 
 	private Double loanLimit;
 	
@@ -28,7 +28,7 @@ public class BusinessAccount extends Account {
 	}
 	
 	@Override
-	public void withdraw(double amount) {
+	public final void withdraw(double amount) {
 		super.withdraw(amount);
 		balance -= 2.0;
 	}
